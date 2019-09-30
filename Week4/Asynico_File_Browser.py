4d8964dfdae0b0ebd7b63570067eecaa3bfb5b8f
Certseeds <nanoseedskc@gmail.com>
2019-09-30 13:28:28 +0800

    version 1.0

--- Week4/Asynico_File_Browser.py
+++ Week4/Asynico_File_Browser.py
@@ -1,29 +1,30 @@
 import asyncio
 import os
 import mimetypes
+import codecs
 
 head = [b'HTTP/1.0 200 OK\r\n',
-         b'Connection: close'
-         b'Content-Type:text/html; charset=utf-8\r\n',
-         b'\r\n',
-         b'<html><body>There just include HEAD message<body></html>\r\n',
-         b'\r\n']
+        b'Connection: close'
+        b'Content-Type:text/html; charset=utf-8\r\n',
+        b'\r\n',
+        b'<html><body>There just include HEAD message<body></html>\r\n',
+        b'\r\n']
 err404 = [b'HTTP/1.0 404 Not Found\r\n',
-          b'Connection: close' 
+          b'Connection: close'
           b'Content-Type:text/html; charset=utf-8\r\n',
           b'\r\n',
           b'<html><body>404 Not Found<body></html>\r\n',
           b'\r\n']
 err405 = [b'HTTP/1.0 405 Method Not Allowed\r\n',
-          b'Connection: close' 
+          b'Connection: close'
           b'Content-Type:text/html; charset=utf-8\r\n',
           b'\r\n',
           b'<html><body>405 Method Not Allowed<body></html>\r\n',
           b'\r\n']
 list_file = [b'HTTP/1.0 200 OK\r\n',
-         b'Connection: close'
-         b'Content-Type:text/html; charset=utf-8\r\n',
-         b'\r\n',
+             b'Connection: close\r\n',
+             b'Content-Type:text/html; charset=utf-8\r\n',
+             b'\r\n',
              b'<html><head><title>Index of .//</title></head>',
              b'<body bgcolor="white"> ',
              b'<h1>Index of .//</h1><hr> ',
@@ -31,8 +32,14 @@ list_file = [b'HTTP/1.0 200 OK\r\n',
              b'</pre> ',
              b'<hr> ',
              b'\r\n']
+files = [b'HTTP/1.0 200 OK\r\n',
+         b'Connection: close\r\n',
+         b'Content-Type:text/html; charset=utf-8\r\n',
+         b'\r\n',
+         b'\r\n']
 server_address = '127.0.0.1'
 server_port = 11451
+
 
 def echo():
     echo_loop = asyncio.get_event_loop()
@@ -49,52 +56,75 @@ def echo():
 
 
 async def thinkpeach(reader, writer):
-    data = await reader.readline(11451)
-    msg = data.decode().split(' ')
-    dir_list = os.listdir()
-    method = msg[0][0]
+    print("comes!!!")
+    data = await reader.read(11451)
+    datas = data.decode().split('\r\n')
+    msg = datas[0].split(' ')
+    method = msg[0]
+    path = msg[1]
+    print(method)
+    print(path)
+    # for i in datas:
+    #   print(i)
     willreturn = list_file.copy()
     if data:
         if method not in ('GET', 'HEAD'):
-            writer.writelines(err405)
-            writer.close
-            return
-        path = msg[0][1]
-        if path != '/' and path[1:] not in dir_list:
-            writer.writelines(err404)
-            await writer.drain()
-            writer.close()
+            print("not in here")
+            willreturn = err405
         else:
-            os.chdir(path[1:])
-            new_path_list = os.listdir()
-            dir_path = []
-            file_path = []
-            for i in new_path_list:
-                if os.path.isfile(i):
-                    file_path.append("<a href=\"{}\">{}/</a><br>".format(i,i).encode('utf-8'))
-                if os.path.isdir(i):
-                    dir_path.append("<a href=\"{}\">{}</a><br>".format(i,i).encode('utf-8'))
-            for i in dir_path:
-                willreturn.insert(i, -3)
-            for i in file_path:
-                willreturn.insert(i, -3)
-
-
-
-        writer.writelines()
-        print(data)
+            if path == '/':  # 本目录
+                print("本目录")
+                dir_list = os.listdir()
+                await get_body(dir_list, willreturn, '')
+            elif os.path.isdir(path[1:]):
+                print("是目录")
+                gang_number = path.count('/')
+                os.chdir(path[1:])
+                new_path_list = os.listdir()
+                willreturn[4] = "<html><head><title>Index of {}</title></head>".format(path[1:]).encode('utf-8')
+                await get_body(new_path_list, willreturn, path[1:] + '/')
+                for i in range(0, gang_number):
+                    os.chdir("..")
+            elif os.path.isfile(path[1:]):
+                print("是文件")
+                file = open(path[1:], 'r', encoding='utf-8')
+                willreturn = files.copy()
+                willreturn[2] = "Content-Type:{}; charset=utf-8\r\n".format(mimetypes.guess_type(path[1:])[0]).encode('utf-8')
+                willreturn.insert(-1, "{}\r\n".format(file.read()).encode('utf-8'))
+            else:  # 什么也不是
+                print("啥也不是")
+                willreturn = err404
+        if method == 'HEAD':
+            print("是HEAD")
+            writer.writelines(willreturn[0:4])
+        else:
+            writer.writelines(willreturn)
         await writer.drain()
+        writer.close()
+        print("now it is in here")
+        return
     else:
         writer.close()
         print("this process jump out")
         return
 
 
-async def collcet():
-    list = os.listdir()
-    for i in list:
-        i = '.'+i
-    return list
+async def get_body(path_list, willreturn, adden):
+    dir_path = []
+    file_path = []
+    for i in path_list:
+        if os.path.isfile(i):
+            file_path.append("<a href=\"{}\">{}/</a><br>".format((adden + i), i).encode('utf-8'))
+        elif os.path.isdir(i):
+            dir_path.append("<a href=\"{}\">{}</a><br>".format((adden + i), i).encode('utf-8'))
+    willreturn.insert(-3, "<a href=\"{}\">{}</a><br>".format(adden + "../", "../").encode('utf-8'))
+    for i in file_path:
+        willreturn.insert(-3, i)
+    for i in dir_path:
+        willreturn.insert(-3, i)
+    return willreturn
+
+
 if __name__ == "__main__":
     try:
         echo()
--- Week4/testing_3.py
+++ Week4/testing_3.py
@@ -1,13 +1,23 @@
 import os
-print (b'HEAD' in (b'GET',b'HEAD'))
+import mimetypes
+
+
+def test_fun():
+    os.chdir("test_dir")
+
+
+def test_fun_2():
+    os.chdir("..")
+
+
+print(b'HEAD' in (b'GET', b'HEAD'))
 list = os.listdir()
 for i in list:
-    i =  i
+    i = i
     print(i)
 print(list[0][1:])
 print(os.path.isfile("/file2.py"[1:]))
 print(os.path.isdir("test_dir"))
-os.chdir("test_dir")
 for i in os.listdir():
     print(1)
     print(i)
@@ -15,9 +25,27 @@ print('ok')
 string = "this is a test{}".format("hanpi")
 print(string)
 i = "tesingadhasdkj"
-print("<a href=\"{}\">{}/</a><br>".format(i,i))
-test_3 = "<a href=\"{}\">{}/</a><br>".format(i,i).encode('utf-8')
+print("<a href=\"{}\">{}/</a><br>".format(i, i))
+test_3 = "<a href=\"{}\">{}/</a><br>".format(i, i).encode('utf-8')
 test_4 = b'<a href="tesingadhasdkj">tesingadhasdkj/</a><br>'
 print(test_3)
 print(test_4)
 print(test_3 == test_4)
+
+list = os.listdir()
+for i in list:
+    print(i)
+test_fun()
+list = os.listdir()
+for i in list:
+    print(i)
+test_fun_2()
+list = os.listdir()
+for i in list:
+    print(i)
+print(os.path.isdir("test_dir/test_dir_2"))
+print(os.path.isfile("test_dir/test"))
+file = open("test_dir/test")
+print(file.read())
+file.close()
+print(mimetypes.guess_type("a.txt")[0])
