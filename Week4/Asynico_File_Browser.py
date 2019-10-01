c08392771b7993de6c545ac2b0bbe1250847b890
Certseeds <nanoseedskc@gmail.com>
2019-10-01 18:53:35 +0800

    Absolutely failed, Version 1.1

--- Week4/Asynico_File_Browser.py
+++ Week4/Asynico_File_Browser.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import mimetypes
+import urllib.parse
 import codecs
 
 head = [b'HTTP/1.0 200 OK\r\n',
@@ -25,18 +26,24 @@ list_file = [b'HTTP/1.0 200 OK\r\n',
              b'Connection: close\r\n',
              b'Content-Type:text/html; charset=utf-8\r\n',
              b'\r\n',
-             b'<html><head><title>Index of .//</title></head>',
-             b'<body bgcolor="white"> ',
-             b'<h1>Index of .//</h1><hr> ',
-             b'<pre>',
-             b'</pre> ',
-             b'<hr> ',
+             b'<!DOCTYPE html>\r\n<html><head><title>Index of .//</title></head>\r\n',
+             b'<body bgcolor="white"> \r\n',
+             b'<h1>Index of .//</h1><hr>\r\n ',
+             b'<pre>\r\n',
+             b'</pre>\r\n ',
+             b'<hr> \r\n',
              b'\r\n']
 files = [b'HTTP/1.0 200 OK\r\n',
          b'Connection: close\r\n',
          b'Content-Type:text/html; charset=utf-8\r\n',
          b'\r\n',
          b'\r\n']
+image = [b'HTTP/1.0 200 OK\r\n',
+         b'Connection: close\r\n',
+         b'Content-Type:text/html; charset=utf-8\r\n',
+         b'\r\n',
+         b'\r\n']
+path_origin = os.path.dirname(__file__)
 server_address = '127.0.0.1'
 server_port = 11451
 
@@ -62,14 +69,14 @@ async def thinkpeach(reader, writer):
     msg = datas[0].split(' ')
     method = msg[0]
     path = msg[1]
-    print(method)
-    print(path)
-    # for i in datas:
-    #   print(i)
+    if path[len(path)-1]!='/':
+        path += '/'
+    for i in datas:
+       print(i)
     willreturn = list_file.copy()
     if data:
+        print(os.getcwd())
         if method not in ('GET', 'HEAD'):
-            print("not in here")
             willreturn = err405
         else:
             if path == '/':  # 本目录
@@ -77,25 +84,38 @@ async def thinkpeach(reader, writer):
                 dir_list = os.listdir()
                 await get_body(dir_list, willreturn, '')
             elif os.path.isdir(path[1:]):
+                domains = path.split("/")
                 print("是目录")
-                gang_number = path.count('/')
-                os.chdir(path[1:])
+                print(path[0:len(path) - len(domains[len(domains)-1])])
+                os.chdir('.'+path)
                 new_path_list = os.listdir()
-                willreturn[4] = "<html><head><title>Index of {}</title></head>".format(path[1:]).encode('utf-8')
-                await get_body(new_path_list, willreturn, path[1:] + '/')
-                for i in range(0, gang_number):
-                    os.chdir("..")
-            elif os.path.isfile(path[1:]):
+                willreturn[4] = "<!DOCTYPE html>\r\n<html><head><title>Index of {}</title></head>".format(path[1:]).encode('utf-8')
+                willreturn[6] = "<h1>Index of {}</h1><hr>".format(path[1:]).encode('utf-8')
+                willreturn.insert(-3, "<a href=\"{}\">{}</a><br>\r\n".format(path[0:len(path) - len(domains[len(domains)-2]) - 1], "../").encode('utf-8'))
+                await get_body(new_path_list, willreturn, path)
+                os.chdir(path_origin)
+            elif os.path.isfile(path[1:-1]):
+                path = path[0:-1]
                 print("是文件")
-                file = open(path[1:], 'r', encoding='utf-8')
+                file = open('.'+path, 'r', encoding='utf-8')
                 willreturn = files.copy()
-                willreturn[2] = "Content-Type:{}; charset=utf-8\r\n".format(mimetypes.guess_type(path[1:])[0]).encode('utf-8')
-                willreturn.insert(-1, "{}\r\n".format(file.read()).encode('utf-8'))
+                file_type = mimetypes.guess_type(path[1:])[0]
+                if file_type.split("/")[0] == "audio":
+                    willreturn[2] = "Content-Type:{}; charset=utf-8; \r\nContent-Length:{}\r\n".format(file_type, os.path.getsize(path[1:])).encode('utf-8')
+                    #willreturn.insert(-2, "<embed height=\"50\" width=\"100\" src=\"{}\"\r\n>".format(path[1:]).encode('utf-8'))
+                    willreturn.insert(-2, "<audio controls=\"controls\" height=\"100\" width=\"100\" autoplay=\"autoplay\">".encode('utf-8'))
+                    willreturn.insert(-2, "<source src=\"{}\" type=\"{}\">".format(path[1:], file_type).encode('utf-8'))
+                    willreturn.insert(-2, "</audio>".encode('utf-8'))
+                # elif file_type.split("/")[0] == "vedio":
+                # elif file_type.split("/")[0] == "audio":
+                else:
+                    willreturn[2] = "Content-Type:{}; charset=utf-8\r\n ".format(file_type).encode('utf-8')
+                    willreturn.insert(2, "Content-Length:{};\r\n".format(os.path.getsize(path[1:])).encode('utf-8'))
+                    willreturn.insert(-1, "{}\r\n".format(file.read()).encode('utf-8'))
             else:  # 什么也不是
                 print("啥也不是")
                 willreturn = err404
         if method == 'HEAD':
-            print("是HEAD")
             writer.writelines(willreturn[0:4])
         else:
             writer.writelines(willreturn)
@@ -113,11 +133,11 @@ async def get_body(path_list, willreturn, adden):
     dir_path = []
     file_path = []
     for i in path_list:
+        print(i)
         if os.path.isfile(i):
-            file_path.append("<a href=\"{}\">{}/</a><br>".format((adden + i), i).encode('utf-8'))
+            file_path.append("<a href=\"{}\">{}</a><br\r\n>".format((adden + i), i).encode('utf-8'))
         elif os.path.isdir(i):
-            dir_path.append("<a href=\"{}\">{}</a><br>".format((adden + i), i).encode('utf-8'))
-    willreturn.insert(-3, "<a href=\"{}\">{}</a><br>".format(adden + "../", "../").encode('utf-8'))
+            dir_path.append("<a href=\"{}\">{}</a><br>\r\n".format((adden + i+"/"), i+"/").encode('utf-8'))
     for i in file_path:
         willreturn.insert(-3, i)
     for i in dir_path:
